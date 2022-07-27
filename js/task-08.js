394e71b706c3830053d141928d3c677b4bdd417e
USAenko <serhiiusenkoa@gmail.com>
2022-07-27 21:31:44 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(action) {
+    action.preventDefault();
+    const formElements = action.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    
+    console.log(mail, password);
+}
