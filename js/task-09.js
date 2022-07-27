e0a642e4d54856a6b501f282858de7e0081b64e2
USAenko <serhiiusenkoa@gmail.com>
2022-07-27 20:56:22 +0300

    task09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+const pEl = document.querySelector('.widget p')
+
+buttonEl.addEventListener('click', colorChanger);
+
+function colorChanger() {
+  pEl.style.color = getRandomHexColor();
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  console.log(color)  
+  spanEl.textContent = color;
+  
+}
